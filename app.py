cb0a3dcdbe0bba14af2b7d884004de2ca8d99fe6
TalTouboul <taltub123@gmail.com>
2025-02-25 13:52:25 +0200

    something to commit

--- app.py
+++ app.py
@@ -220,7 +220,9 @@ def facebook_login():
 
 # === RUN APP ===
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 #python -m venv venv
