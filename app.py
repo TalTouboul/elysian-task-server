8880720614afba34589430de94c8a7c8c4b32692
TalTouboul <taltub123@gmail.com>
2025-02-26 01:08:37 +0200

    something to commit2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from pymongo import MongoClient
 from mongoengine import Document, StringField, EmailField
 from random import randint
